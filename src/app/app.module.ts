6da25d8ae16476857065fd78a3a6a3dc37fe0650
Matthew Paice <matthew@paicemedia.co.uk>
2020-12-22 13:05:02 +0000

    Initial Project Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AccordionComponent } from './components/accordion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccordionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
